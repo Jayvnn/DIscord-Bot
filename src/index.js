d7249f9468fb4e8280ff8df8f92abf33b3f7aa0d
Jayden Nguyen <Jayden.vinh.nguyen@gmail.com>
2024-08-18 23:20:54 -0700

    Update package.json and index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //Imported Packages
 const {Client, IntentsBitField} = require('discord.js');
-
+require('dotenv').config();
 
 const client = new Client({
     //Intents are a set of permissions that the bot can use to get access to set of events
@@ -9,13 +9,27 @@ const client = new Client({
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
-    ]
-})
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
 
+client.on('ready', (clientUser) => {
+    console.log(`${clientUser.user.tag} is online`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'hello') {
+        message.reply('Hey there! :D');
+    }
+})
+client.login(
+    process.env.BOT_TOKEN
+);
 
 /*
 
 |----Dev Notes----| 
 Step 1: npm run build
+Step 2: npm run dev
 
 */
